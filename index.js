be34d5cb9e6c3e519b763cabb2b921a63c31903e
Jamie Nichols <codingbutter@gmail.com>
2020-12-18 04:33:46 -0500

    Removed SSL info updated the env.example to show example for cert and key

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const DotEnv = require("dotenv");
 const RconApi = require("./RconApi");
 const fs = require("fs");
 const https = require("https");
-const path = require("path");
 DotEnv.config();
 const app = Express();
 
 const httpsOptions = {
-    cert: fs.readFileSync(path.joint(__dirname, 'ssl', 'server.crt')),
-    key: fs.readFileSync(path.join(__dirname,'ssl','server.key'))
+    cert: fs.readFileSync(process.env.SSL_CERT),
+    key: fs.readFileSync(process.env.SSL_KEY)
 }
 
 RconApi(app);
 
 https.createServer(httpsOptions, app)
     .listen(process.env.PORT, () => {
-    const log(`Serving on port ${process.env.PORT}`)
+    console.log(`Serving on port ${process.env.PORT}`)
 })
